753eac99638f344af0e301fe5fa747f21756c95a
agustinntarias <gustin444@gmail.com>
2021-01-11 13:43:11 -0300

    Changed http to https

--- src/logic/fetchRandomQuote.js
+++ src/logic/fetchRandomQuote.js
@@ -1,5 +1,5 @@
 export default async function fetchRandomQuote() {
-  const url = "http://api.quotable.io/random";
+  const url = "https://api.quotable.io/random";
   const response = await fetch(url);
   const data = await response.json();
   return data;
