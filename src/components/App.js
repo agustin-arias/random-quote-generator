9e01089f214617d1f7b0aa3df776221d6473bd4c
Agustin Arias <agustin.arias@trebol-it.cl>
2023-02-11 21:35:00 -0300

    Added DefaultQuotes array

--- src/components/App.js
+++ src/components/App.js
@@ -2,50 +2,47 @@ import React, { Component } from "react";
 import changeBodyColor from "../logic/changeBodyColor";
 import fetchRandomQuote from "../logic/fetchRandomQuote";
 import LinkToPage from "./LinkToPage";
+import DefaultQuotes from "../logic/defaultQuotes";
 import QuoteBox from "./QuoteBox";
 
+const index = Math.floor(Math.random() * DefaultQuotes.length);
 export default class App extends Component {
-  state = {
-    color: "",
-    randomQuoteData: {
-      author: "",
-      content: "",
-    },
-  };
+    state = {
+        color: changeBodyColor(),
+        randomQuoteData: DefaultQuotes[index],
+    };
 
-  async fadeOutInText() {
-    const text = document.getElementById("text");
-    text.classList.add("fadeOutIn");
-    setTimeout(() => {
-      text.classList.remove("fadeOutIn");
-    }, 1000);
-  }
+    async fadeOutInText() {
+        const text = document.getElementById("text");
+        text.classList.add("fadeOutIn");
+        setTimeout(() => {
+            text.classList.remove("fadeOutIn");
+        }, 1000);
+    }
 
-  async handleNewQuote() {
-    await this.fadeOutInText();
-    const { author, content } = await fetchRandomQuote();
-    const color = changeBodyColor();
-    this.setState({ color, randomQuoteData: { author, content } });
-  }
+    async handleNewQuote() {
+        await this.fadeOutInText();
+        const { author, content } = await fetchRandomQuote();
+        const color = changeBodyColor();
+        this.setState({ color, randomQuoteData: { author, content } });
+    }
 
-  async componentDidMount() {
-    await this.handleNewQuote();
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <QuoteBox
-          color={this.state.color}
-          onNewQuote={() => {
-            this.handleNewQuote();
-          }}
-          randomQuoteData={this.state.randomQuoteData}
-        />
-        <LinkToPage
-          url={"https://github.com/agustinntarias/random-quote-generator"}
-        />
-      </React.Fragment>
-    );
-  }
+    render() {
+        return (
+            <React.Fragment>
+                <QuoteBox
+                    color={this.state.color}
+                    onNewQuote={() => {
+                        this.handleNewQuote();
+                    }}
+                    randomQuoteData={this.state.randomQuoteData}
+                />
+                <LinkToPage
+                    url={
+                        "https://github.com/agustinntarias/random-quote-generator"
+                    }
+                />
+            </React.Fragment>
+        );
+    }
 }
---
+++ src/logic/defaultQuotes.js
@@ -0,0 +1,24 @@
+export default [
+    {
+        author: "Edith Wharton",
+        content:
+            "There are two ways of spreading light: to be the candle or the mirror that reflects it.",
+    },
+    {
+        author: "Benjamin Disraeli",
+        content: "The secret of success is constancy to purpose.",
+    },
+    {
+        author: "Bernard Shaw",
+        content:
+            "We are made wise not by the recollection of our past, but by the responsibility for our future.",
+    },
+    {
+        author: "Joe Namath",
+        content: "If you aren't going all the way, why go at all?",
+    },
+    {
+        author: "Francis Bacon",
+        content: "Wise men make more opportunities than they find.",
+    },
+];
